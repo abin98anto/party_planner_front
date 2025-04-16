68150e43a1735c31e90ab5c61e26145c4c799240
abin98anto <abin98anto@gmail.com>
2025-04-16 09:21:46 +0530

    base url changed in axios

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://party-planner-nodejs.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
