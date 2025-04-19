7b36234c221c503df0d6c7b4eba8e243ab819f70
abin98anto <abin98anto@gmail.com>
2025-04-19 10:21:29 +0530

    modified unprotected routes

--- src/components/common/ProtectedRoutes/UnProtectedRoute.tsx
+++ src/components/common/ProtectedRoutes/UnProtectedRoute.tsx
@@ -1,14 +1,11 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { AppRootState } from "../../../redux/store";
 
 const UnProtectedRoute = () => {
   const userInfo = useSelector((state: AppRootState) => state.user.userInfo);
-  const location = useLocation();
-  const currentUrl = `${window.location.origin}${location.pathname}${location.search}${location.hash}`;
-  const where = currentUrl.split("/")[3] === "admin" ? "admin" : "";
 
-  return !userInfo ? <Outlet /> : <Navigate to={`/${where}`} />;
+  return !userInfo ? <Outlet /> : <Navigate to={`/`} />;
 };
 
 export default UnProtectedRoute;
